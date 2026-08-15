924e0dc58401b73c2bbd507ac1a1bcfec9c47f26
abraamonsy <boboobo483@gmail.com>
2026-08-15 09:05:16 +0300

    Create menu screen and food item layout

---
+++ app/src/main/java/com/example/restaurantproject/FoodAdapter.java
@@ -0,0 +1,115 @@
+package com.example.restaurantproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.PopupMenu;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
+
+    private List<Food> foodList;
+    private List<Food> allFoodList;
+
+    public FoodAdapter(List<Food> foodList) {
+        this.foodList = foodList;
+        this.allFoodList = new ArrayList<>(foodList);
+    }
+    public void filterByCategory(String category) {
+
+        foodList.clear();
+
+        if (category.equals("All")) {
+
+            foodList.addAll(allFoodList);
+
+        } else {
+
+            for (Food food : allFoodList) {
+
+                if (food.getCategory().equals(category)) {
+                    foodList.add(food);
+                }
+            }
+        }
+
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_food, parent, false);
+
+        return new FoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+
+        Food food = foodList.get(position);
+
+        holder.tvFoodName.setText(food.getName());
+        holder.tvFoodDescription.setText(food.getDescription());
+        holder.tvFoodPrice.setText(food.getPrice() + " EGP");
+        holder.ivFood.setImageResource(food.getImage());
+        holder.btnFoodMenu.setOnClickListener(v -> {
+
+            PopupMenu popupMenu = new PopupMenu(v.getContext(), holder.btnFoodMenu);
+
+            popupMenu.getMenuInflater().inflate(
+                    R.menu.food_popup_menu,
+                    popupMenu.getMenu()
+            );
+
+            popupMenu.setOnMenuItemClickListener(item -> {
+
+                if (item.getItemId() == R.id.action_view_details) {
+                    // TODO: Open Food Details
+                    return true;
+                }
+
+                if (item.getItemId() == R.id.action_add_to_cart) {
+                    // TODO: Add food to Cart
+                    return true;
+                }
+
+                return false;
+            });
+
+            popupMenu.show();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodList.size();
+    }
+
+    public static class FoodViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivFood;
+        TextView tvFoodName;
+        TextView tvFoodDescription;
+        TextView tvFoodPrice;
+        ImageButton btnFoodMenu;
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivFood = itemView.findViewById(R.id.ivFood);
+            tvFoodName = itemView.findViewById(R.id.tvFoodName);
+            tvFoodDescription = itemView.findViewById(R.id.tvFoodDescription);
+            tvFoodPrice = itemView.findViewById(R.id.tvFoodPrice);
+            btnFoodMenu = itemView.findViewById(R.id.btnFoodMenu);
+        }
+    }
+}
--- app/src/main/java/com/example/restaurantproject/MainActivity.java
+++ app/src/main/java/com/example/restaurantproject/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.restaurantproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +24,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerFood = findViewById(R.id.recyclerFood);
+
+        List<Food> foodList = new ArrayList<>();
+
+        foodList.add(new Food(
+                "Classic Burger",
+                "Beef burger with cheese",
+                150,
+                "Burger",
+                R.drawable.ic_launcher_foreground
+        ));
+
+        foodList.add(new Food(
+                "Chicken Pizza",
+                "Chicken pizza with cheese",
+                180,
+                "Pizza",
+                R.drawable.ic_launcher_foreground
+        ));
+
+        foodList.add(new Food(
+                "Pasta",
+                "Creamy pasta with chicken",
+                120,
+                "Pasta",
+                R.drawable.ic_launcher_foreground
+        ));
+
+        FoodAdapter adapter = new FoodAdapter(foodList);
+
+        recyclerFood.setLayoutManager(new LinearLayoutManager(this));
+        recyclerFood.setAdapter(adapter);
+        Spinner spinnerCategory = findViewById(R.id.spinnerCategory);
+
+        String[] categories = {
+                "All",
+                "Burger",
+                "Pizza",
+                "Pasta"
+        };
+        ArrayAdapter<String> spinnerAdapter =
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_spinner_item,
+                        categories
+                );
+
+        spinnerAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        spinnerCategory.setAdapter(spinnerAdapter);
+        spinnerCategory.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(
+                            AdapterView<?> parent,
+                            View view,
+                            int position,
+                            long id) {
+
+                        String selectedCategory =
+                                parent.getItemAtPosition(position).toString();
+
+                        adapter.filterByCategory(selectedCategory);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                }
+        );
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
